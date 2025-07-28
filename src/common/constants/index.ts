8bac584978c1896dd61da62432e8ca0c1f93b2c4
ACAZUMAH <calebazumah9@gmail.com>
2025-07-28 13:06:02 +0000

    updates

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,4 +1,4 @@
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const isProduction = process.env.NODE_ENV === "production";
 
-export const whitelist = ["https://mini-skill-swap.netlify.app", "http://localhost:5173/"];
+export const whitelist = ["https://mini-skill-swap.netlify.app", "http://localhost:5173"];
