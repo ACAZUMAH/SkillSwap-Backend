dc46f6174d174eebaa63dcb923c45a59b373e0ec
ACAZUMAH <calebazumah9@gmail.com>
2025-08-12 17:31:03 +0000

    updates

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -14,26 +14,27 @@ const cronJobs: CronJobConfig[] = [
   },
 ];
 
-
 export const applyCronJobs = () => {
-    cronJobs.map((jobConfig) => {
-        logger.info(`[Cron] Starting ${jobConfig.name}`);
-        const job = new CronJob(
-            jobConfig.schedule,
-            async () => {
-                try {
-                    await jobConfig.job();
-                    logger.info(`[Cron] Completed ${jobConfig.name}`);
-                } catch (error) {
-                    logger.error(`[Cron] Failed ${jobConfig.name}:`, error);
-                }
-            },
-            null,
-            true, // Start the job immediately
-            "UTC", // Timezone
-            undefined,
-            jobConfig.start,
-        );
-        job.start();
-    })
-}
+  cronJobs.map((jobConfig) => {
+    logger.info(`[Cron] Starting ${jobConfig.name}`);
+
+    const job = new CronJob(
+      jobConfig.schedule,
+      async () => {
+        try {
+          await jobConfig.job();
+          logger.info(`[Cron] Completed ${jobConfig.name}`);
+        } catch (error) {
+          logger.error(`[Cron] Failed ${jobConfig.name}:`, error);
+        }
+      },
+      null,
+      true, // Start the job immediately
+      "UTC", // Timezone
+      undefined,
+      jobConfig.start
+    );
+    
+    job.start();
+  });
+};
